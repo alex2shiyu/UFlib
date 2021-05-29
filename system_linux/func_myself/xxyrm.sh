840946b79004240e4f350de5d1ef09cd70bf535a
alex2shiyu <sypeng@iphy.ac.cn>
2021-05-29 15:24:37 +0800

    add mkdir Trash in xxyrm.sh

--- system_linux/func_myself/xxyrm.sh
+++ system_linux/func_myself/xxyrm.sh
@@ -2,6 +2,14 @@
 
 TRASH_DIR=$HOME/.Trash
 
+
+if [[ ! -e $TRASH_DIR ]]
+then
+	mkdir $TRASH_DIR
+        echo "creat $TRASH_DIR"
+fi
+
+
 if [ $# -eq 0 ]; then
         echo "usage: xxyrm.sh <files...>" >&2
         exit 2;
